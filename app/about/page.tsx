afe035db1f403fcf4badc27f494e0bbc7b657688
Shashank RM <shashankreddy0608@gmail.com>
2025-05-17 23:23:46 +0530

    placeholder content in about page

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+       <p className="text-2xl"> Page Content is Coming Soon...</p>
+    </div>
+  )
+}
+
+export default page
